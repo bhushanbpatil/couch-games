82dca407c0b245f131884dfdfad2527cd8959e1a
bhution <bhushanbpatil8@gmail.com>
2026-07-04 17:52:01 -0500

    Initial Commit

---
+++ Couch Games/Couch_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Couch_GamesApp.swift
+//  Couch Games
+//
+//  Created by Bhushan Patil on 04/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Couch_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
